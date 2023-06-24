b0921e057337e3e302314e544e5eeab0eb3d6a1f
Andhikap <prameswaara@gmail.com>
2023-06-24 16:00:54 +0700

    update Peminjaman

--- src/com/belajar/asoc/studikasus/Main.java
+++ src/com/belajar/asoc/studikasus/Main.java
@@ -3,18 +3,30 @@ package com.belajar.asoc.studikasus;
 import java.time.LocalDate;
 
 /**
- * Main
+ * Asosiasi Objek = Hubungan antara objek
+ * Agregasi
+ * - Hubungan antara dua objek dimana satu objek berperan sebagai container
+ *   objek lain
+ * - Objek yang ditampung bisa ada secara independent tidak bergantung ke objek
+ *   yang menampung dan dapat terhubung dengan objek lain atau dengan objek
+ *   pemilik yang berbeda.
+ * - Contoh: Hubungan antara Anggota dan Peminjaman dalam contoh kasus peminjaman
+ *   buku. Seorang Anggota dapat memiliki banyak Peminjaman, tetapi Peminjaman
+ *   juga dapat ada secara independen tanpa tergantung pada Anggota.
+ * 
+ * 
+ * 
  */
 public class Main {
 
     public static void main(String[] args) {
         Anggota anggota = new Anggota("Andhika", "Jakarta", 1);
         Buku buku1 = new Buku("Java OOP", "James Gosling", "A121");
-        Buku buku2 = new Buku("Java Spring", "James Gosling", "A121");
-        Buku buku3 = new Buku("Java JDBC", "James Gosling", "A121");
-        Peminjaman peminjaman1 = new Peminjaman(buku1, anggota, LocalDate.of(2000, 5, 23));
-        Peminjaman peminjaman2 = new Peminjaman(buku2, anggota, LocalDate.of(2000, 5, 23));
-        Peminjaman peminjaman3 = new Peminjaman(buku3, anggota, LocalDate.of(2000, 5, 23));
+        Buku buku2 = new Buku("Java Spring", "James Gosling", "A122");
+        Buku buku3 = new Buku("Java JDBC", "James Gosling", "A123");
+        Peminjaman peminjaman1 = new Peminjaman(buku1, LocalDate.of(2000, 5, 23));
+        Peminjaman peminjaman2 = new Peminjaman(buku2, LocalDate.of(2000, 5, 23));
+        Peminjaman peminjaman3 = new Peminjaman(buku3, LocalDate.of(2000, 5, 23));
         anggota.setPeminjaman(peminjaman1);
         anggota.setPeminjaman(peminjaman2);
         anggota.setPeminjaman(peminjaman3);
--- src/com/belajar/asoc/studikasus/Peminjaman.java
+++ src/com/belajar/asoc/studikasus/Peminjaman.java
@@ -4,18 +4,11 @@ import java.time.LocalDate;
 
 public class Peminjaman {
     private Buku buku;
-    private Anggota anggota;
     private LocalDate tanggalPeminjaman;
 
-    public Peminjaman(Buku buku, Anggota anggota) {
+    public Peminjaman(Buku buku, LocalDate localDate) {
         this.buku = buku;
-        this.anggota = anggota;
-    }
-
-    public Peminjaman(Buku buku, Anggota anggota, LocalDate tanggalPeminjaman) {
-        this.buku = buku;
-        this.anggota = anggota;
-        this.tanggalPeminjaman = tanggalPeminjaman;
+        this.tanggalPeminjaman = localDate;
     }
 
     public Buku getBuku() {
@@ -24,14 +17,6 @@ public class Peminjaman {
 
     public void setBuku(Buku buku) {
         this.buku = buku;
-    }
-
-    public Anggota getAnggota() {
-        return anggota;
-    }
-
-    public void setAnggota(Anggota anggota) {
-        this.anggota = anggota;
     }
 
     public LocalDate getTanggalPeminjaman() {
